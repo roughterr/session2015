a797bc003177bd8661676320457d6274e6726850
Misha <misha@nau.nau>
2015-10-31 01:23:14 +0200

    adding a book

--- csharp/coursework/Marthe/Marthe/Form1.cs
+++ csharp/coursework/Marthe/Marthe/Form1.cs
@@ -7,16 +7,23 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
+using System.Collections;
+
 namespace Marthe
 {
     public partial class Form1 : Form
     {
+        private static ArrayList allTheBooks { get; set; }
+
         private static ShowAllBooksForm showAllBooksForm;
 
         public Form1()
         {
             InitializeComponent();
             showAllBooksForm = new ShowAllBooksForm();
+            allTheBooks = new ArrayList();
+            Book book1 = new Book("David Copperfield", "Charles Dickens");
+            allTheBooks.Add(book1);
         }
 
         private void button1_Click(object sender, EventArgs e)
--- csharp/coursework/Marthe/Marthe/ShowAllBooks.cs
+++ csharp/coursework/Marthe/Marthe/ShowAllBooks.cs
@@ -8,6 +8,44 @@ namespace Marthe
 {
     class ShowAllBooksForm : Form
     {
+        private RichTextBox richTextBox1;
+    
+        public ShowAllBooksForm()
+        {
+            InitializeComponent();
+        }
 
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(258, 96);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "awwawa";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // ShowAllBooksForm
+            // 
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "ShowAllBooksForm";
+            this.ResumeLayout(false);
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
