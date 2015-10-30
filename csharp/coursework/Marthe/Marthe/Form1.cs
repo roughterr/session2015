0d33d38973f69d247160a67434cf8d8d5eae77a0
Misha <misha@nau.nau>
2015-10-31 01:11:01 +0200

    added a second window

--- csharp/coursework/Marthe/Marthe/Form1.cs
+++ csharp/coursework/Marthe/Marthe/Form1.cs
@@ -11,9 +11,17 @@ namespace Marthe
 {
     public partial class Form1 : Form
     {
+        private static ShowAllBooksForm showAllBooksForm;
+
         public Form1()
         {
             InitializeComponent();
+            showAllBooksForm = new ShowAllBooksForm();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            showAllBooksForm.Show();
         }
     }
 }
